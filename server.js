ff727b09f57c33c01a0430f8bb3c4f93fa088fc5
Davi-mulB <131017124+Davi-mulB@users.noreply.github.com>
2025-04-09 00:27:54 +0000

    Ajeitei coisinhas mínimas

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const myApp = require('./myApp');
 const express = require('express');
 const app = express();
+const path = require('path')
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "index.html"))
+})
+
+app.use(express.static(__dirname));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT)
