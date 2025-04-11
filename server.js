9eb6583a00efdea96a5c9539351ca988de9c3e3c
Davi-mulB <131017124+Davi-mulB@users.noreply.github.com>
2025-04-11 12:57:38 +0000

    ajeitando pastas

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
 const isInvalidDate = (date) => date.toUTCString() === "Invalid Date";
 const isInvalidTimezone = (timezone) => timezone < -12 || timezone > 12;
 const setTimezone = (timezone) => {if(timezone < -12){
@@ -78,7 +74,12 @@ app.get("/api", function(req, res){
     })
 });
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+app.get('/', (req, res) => { 
+    res.sendFile(path.join(__dirname, 'frontend', 'index.html')); 
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
